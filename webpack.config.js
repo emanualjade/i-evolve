62c093f97e767dc4591274b0c559d8e439b3d5dc
Emanual Jade <emanualjade@gmail.com>
2017-03-19 12:24:28 -0400

    Update build

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,9 @@ module.exports = {
       template: 'src/index.html',
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
   ],
 };
