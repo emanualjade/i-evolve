2ebfc1453026a4241d7299fd32b9bc513416f7d6
Emanual Jade <emanualjade@gmail.com>
2017-03-18 23:56:50 -0400

    Attempt webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../style/style.css';
+
+const App = () => (<h1>i-evolve</h1>);
+
+ReactDOM.render(<App />, document.getElementById('root'));
