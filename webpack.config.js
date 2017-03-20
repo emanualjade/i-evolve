ef48946cb7122894427e697f3259a95600bfe6f4
Emanual Jade <emanualjade@gmail.com>
2017-03-19 23:58:11 -0400

    Tooling updates

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,12 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
+      {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: /node_modules/,
