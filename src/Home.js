c7696d957ad9765c5336055da553b8892c441f50
Emanual Jade <emanualjade@gmail.com>
2017-03-19 23:17:37 -0400

    More setup

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const Home = () => <h1>Home</h1>;
+const Home = () => (
+  <div>
+    <h1>i-evolve</h1>
+    <input type="text" placeholder='search' />
+    <a>or Browse All</a>
+  </div>
+);
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -5,20 +5,21 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+import '../style/normalize.css';
 import '../style/style.css';
 import Home from './Home';
 import About from './About';
 
 const App = () => (
   <Router>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-      <hr />
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
+    <div className='app'>
+      <div className='landing'>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <hr />
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </div>
     </div>
   </Router>
 );
