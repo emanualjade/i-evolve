fb327bde63c08aa3c2931e3d9a927364711a2146
Emanual Jade <emanualjade@gmail.com>
2017-04-04 13:24:52 -0400

    Simplify build

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const path = require('path');
 
-const webpackMiddleWare = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.js');
+// const webpackMiddleWare = require('webpack-dev-middleware');
+// const webpack = require('webpack');
+// const webpackConfig = require('./webpack.config.js');
 
 const app = express();
 app.use(express.static('dist'));
 app.use('/public', express.static('./public'));
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(webpackMiddleWare(webpack(webpackConfig)));
-} else {
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.listen(process.env.PORT || 3050);
